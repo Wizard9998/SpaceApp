980ad63770faedd125d89479b58c05c1200ab5d0
Nicolás Meléndez <nico.cap10@hotmail.com>
2021-10-02 15:57:29 -0300

    k

--- SpaceApp/Forms/Seleccion de personaje.Designer.cs
+++ SpaceApp/Forms/Seleccion de personaje.Designer.cs
@@ -129,7 +129,7 @@ namespace SpaceApp.Forms
             this.button1.TabIndex = 9;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+           
             // 
             // dataGridView1
             // 
