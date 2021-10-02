c30ffa0bf22dcbd5ed3021c45f5955533cf51642
Nicolás Meléndez <nico.cap10@hotmail.com>
2021-10-02 01:44:07 -0300

    seqsd

---
+++ SpaceApp/Forms/Preguntas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SpaceApp.Forms
+{
+    public partial class Preguntas : Form
+    {
+        public Preguntas()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- SpaceApp/clases/Fachada.cs
+++ SpaceApp/clases/Fachada.cs
@@ -5,26 +5,44 @@ using System.Text;
 
 namespace SpaceApp.clases
 {
-    class Fachada
+    class Fachada 
     {
         List<Pregunta> preguntas;
         List<Mision> misiones;
-        
-        
+
         public Fachada()
         {
             preguntas = new List<Pregunta>();
             misiones = new List<Mision>();
-         
+           
         }
         public void agregarPregunta(string pregunta, int id_pregunta, string respuesta, int id_respuesta, bool correcto)
         {
             Pregunta p = new Pregunta(pregunta, id_pregunta, respuesta, id_respuesta, correcto);
             preguntas.Add(p);
         }
-        
-
-
-
-    }
+       
+          public void comprobarRespuesta(int distancia, int vida, int respuesta)
+            {
+                if (vida > 0)
+                {
+                    if (respuesta == 1)
+                    {
+                        do
+                        {
+                            distancia = +10;
+                        } while (distancia < 10);
+                    }
+                    else
+                    {
+                        vida = vida - 10;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Te quedaste sin vidas");
+                }
+            }
+     }
 }
+
