86a1cd42becd76b1288f6606fba6f1d32de43624
Rony Palacios <ronypalacios25@gmail.com>
2021-10-02 16:20:47 -0300

    button

--- SpaceApp/Forms/Seleccion de personaje.cs
+++ SpaceApp/Forms/Seleccion de personaje.cs
@@ -43,13 +43,13 @@ namespace SpaceApp.Forms
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        
+
+        private void button1_Click_1(object sender, EventArgs e)
         {
-           
             PantallaCarga p = new PantallaCarga();
             p.Show();
-            this.Close(); 
-
+            this.Close();
         }
     }
 }
