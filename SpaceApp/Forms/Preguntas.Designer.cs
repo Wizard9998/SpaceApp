223c7296bad3b87e5afd6c03c1ab221bc7033d15
Glozard <kevinjoelmoreira4@gmail.com>
2021-10-02 01:55:23 -0300

    aed

--- SpaceApp/Forms/Preguntas.Designer.cs
+++ SpaceApp/Forms/Preguntas.Designer.cs
@@ -94,11 +94,12 @@ namespace SpaceApp.Forms
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(57, 16);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(154, 15);
             this.label1.TabIndex = 5;
             this.label1.Text = "¿Cuantas lunas tiene Marte?";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Preguntas
             // 
