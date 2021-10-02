739cab473b591dd76104e9650519cfdb40c060e3
Rony Palacios <ronypalacios25@gmail.com>
2021-10-01 22:56:47 -0300

    fachada

--- SpaceApp/clases/Fachada.cs
+++ SpaceApp/clases/Fachada.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,18 +9,21 @@ namespace SpaceApp.clases
     {
         List<Pregunta> preguntas;
         List<Mision> misiones;
-
+        
+        
         public Fachada()
         {
             preguntas = new List<Pregunta>();
             misiones = new List<Mision>();
-
+            
         }
         public void agregarPregunta(string pregunta, int id_pregunta, string respuesta, int id_respuesta, int puntaje, bool correcto)
         {
             Pregunta p = new Pregunta(pregunta, id_pregunta, respuesta, id_respuesta, puntaje , correcto);
             preguntas.Add(p);
         }
+
+       
 
     }
 }
