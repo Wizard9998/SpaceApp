5e67ad6d871e7293835ff657aca123b33ac2773a
Nicolás Meléndez <nico.cap10@hotmail.com>
2021-10-02 18:57:13 -0300

    sdfsd

--- SpaceApp/Forms/Fase1.Designer.cs
+++ SpaceApp/Forms/Fase1.Designer.cs
@@ -29,20 +29,27 @@ namespace SpaceApp.Forms
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Fase1));
             this.btnResp1 = new System.Windows.Forms.Button();
             this.btnResp2 = new System.Windows.Forms.Button();
             this.btnResp3 = new System.Windows.Forms.Button();
             this.btnResp4 = new System.Windows.Forms.Button();
-            this.imgBox = new System.Windows.Forms.PictureBox();
             this.lbPregunta = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.imgBox)).BeginInit();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // btnResp1
             // 
-            this.btnResp1.Location = new System.Drawing.Point(126, 299);
+            this.btnResp1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnResp1.BackgroundImage")));
+            this.btnResp1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnResp1.Location = new System.Drawing.Point(23, 165);
             this.btnResp1.Name = "btnResp1";
-            this.btnResp1.Size = new System.Drawing.Size(75, 23);
+            this.btnResp1.Size = new System.Drawing.Size(144, 44);
             this.btnResp1.TabIndex = 0;
             this.btnResp1.Text = "button1";
             this.btnResp1.UseVisualStyleBackColor = true;
@@ -50,65 +57,104 @@ namespace SpaceApp.Forms
             // 
             // btnResp2
             // 
-            this.btnResp2.Location = new System.Drawing.Point(560, 298);
+            this.btnResp2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnResp2.BackgroundImage")));
+            this.btnResp2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnResp2.Location = new System.Drawing.Point(186, 165);
             this.btnResp2.Name = "btnResp2";
-            this.btnResp2.Size = new System.Drawing.Size(75, 23);
+            this.btnResp2.Size = new System.Drawing.Size(144, 44);
             this.btnResp2.TabIndex = 1;
             this.btnResp2.Text = "button2";
             this.btnResp2.UseVisualStyleBackColor = true;
             // 
             // btnResp3
             // 
-            this.btnResp3.Location = new System.Drawing.Point(126, 364);
+            this.btnResp3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnResp3.BackgroundImage")));
+            this.btnResp3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnResp3.Location = new System.Drawing.Point(23, 257);
             this.btnResp3.Name = "btnResp3";
-            this.btnResp3.Size = new System.Drawing.Size(75, 23);
+            this.btnResp3.Size = new System.Drawing.Size(144, 44);
             this.btnResp3.TabIndex = 2;
             this.btnResp3.Text = "button3";
             this.btnResp3.UseVisualStyleBackColor = true;
             // 
             // btnResp4
             // 
-            this.btnResp4.Location = new System.Drawing.Point(560, 364);
+            this.btnResp4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnResp4.BackgroundImage")));
+            this.btnResp4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnResp4.Location = new System.Drawing.Point(186, 257);
             this.btnResp4.Name = "btnResp4";
-            this.btnResp4.Size = new System.Drawing.Size(75, 23);
+            this.btnResp4.Size = new System.Drawing.Size(144, 44);
             this.btnResp4.TabIndex = 3;
             this.btnResp4.Text = "button4";
             this.btnResp4.UseVisualStyleBackColor = true;
             // 
-            // imgBox
-            // 
-            this.imgBox.BackColor = System.Drawing.Color.Transparent;
-            this.imgBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.imgBox.Image = global::SpaceApp.Properties.Resources.PuntaNave;
-            this.imgBox.Location = new System.Drawing.Point(383, 101);
-            this.imgBox.Name = "imgBox";
-            this.imgBox.Size = new System.Drawing.Size(239, 191);
-            this.imgBox.TabIndex = 4;
-            this.imgBox.TabStop = false;
-            // 
             // lbPregunta
             // 
             this.lbPregunta.AutoSize = true;
-            this.lbPregunta.Location = new System.Drawing.Point(314, 37);
+            this.lbPregunta.BackColor = System.Drawing.Color.Transparent;
+            this.lbPregunta.Font = new System.Drawing.Font("Candara", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.lbPregunta.ForeColor = System.Drawing.Color.White;
+            this.lbPregunta.Location = new System.Drawing.Point(54, 41);
             this.lbPregunta.Name = "lbPregunta";
-            this.lbPregunta.Size = new System.Drawing.Size(55, 15);
+            this.lbPregunta.Size = new System.Drawing.Size(104, 29);
             this.lbPregunta.TabIndex = 5;
             this.lbPregunta.Text = "Pregunta";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(456, 165);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(69, 65);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(449, 220);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(88, 64);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.ErrorImage = null;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(435, 257);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(110, 88);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
             // 
             // Fase1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(803, 450);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lbPregunta);
-            this.Controls.Add(this.imgBox);
             this.Controls.Add(this.btnResp4);
             this.Controls.Add(this.btnResp3);
             this.Controls.Add(this.btnResp2);
             this.Controls.Add(this.btnResp1);
+            this.Controls.Add(this.pictureBox2);
             this.Name = "Fase1";
             this.Text = "Fase1";
-            ((System.ComponentModel.ISupportInitialize)(this.imgBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -120,7 +166,9 @@ namespace SpaceApp.Forms
         private System.Windows.Forms.Button btnResp2;
         private System.Windows.Forms.Button btnResp3;
         private System.Windows.Forms.Button btnResp4;
-        private System.Windows.Forms.PictureBox imgBox;
         private System.Windows.Forms.Label lbPregunta;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
