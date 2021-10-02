dbcb8a6e656aaacd5012bfe83bd0033c1847ce3f
Glozard <kevinjoelmoreira4@gmail.com>
2021-10-01 22:22:21 -0300

    agregar pregunta

--- SpaceApp/clases/Fachada.cs
+++ SpaceApp/clases/Fachada.cs
@@ -15,8 +15,11 @@ namespace SpaceApp.clases
             misiones = new List<Mision>();
 
         }
-
-       
+        public void agregarPregunta(string pregunta, int id_pregunta, string respuesta, int id_respuesta, int puntaje, bool correcto)
+        {
+            Pregunta p = new Pregunta(pregunta, id_pregunta, respuesta, id_respuesta, puntaje , correcto);
+            preguntas.Add(p);
+        }
 
     }
 }
