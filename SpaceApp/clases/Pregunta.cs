109822f37b4640e0b15d06bd6d816bc1cd9323d5
Glozard <kevinjoelmoreira4@gmail.com>
2021-10-01 21:50:38 -0300

    Agregar pregunta

--- SpaceApp/clases/Pregunta.cs
+++ SpaceApp/clases/Pregunta.cs
@@ -11,10 +11,17 @@ namespace SpaceApp.clases
         string respuesta = " ";
         int id_respuesta = 0; 
         int puntaje = 0;
+
         bool correcto = true;
-        
+        private string pregunrta;
 
-
-
+        public Pregunta(string pregunrta, int id_pregunta, string respuesta, int id_respuesta, int puntaje)
+        {
+            this.pregunrta = pregunrta;
+            this.id_pregunta = id_pregunta;
+            this.respuesta = respuesta;
+            this.id_respuesta = id_respuesta;
+            this.puntaje = puntaje;
+        }
     }
 }
