79abe38b503f2cddd97f17b8b74e3d80f019af66
Rony Palacios <ronypalacios25@gmail.com>
2021-10-02 21:13:20 -0300

    xcvx

--- SpaceApp/Forms/Fase1.Designer.cs
+++ SpaceApp/Forms/Fase1.Designer.cs
@@ -30,63 +30,18 @@ namespace SpaceApp.Forms
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Fase1));
-            this.btnResp1 = new System.Windows.Forms.Button();
-            this.btnResp2 = new System.Windows.Forms.Button();
-            this.btnResp3 = new System.Windows.Forms.Button();
-            this.btnResp4 = new System.Windows.Forms.Button();
             this.lbPregunta = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnResp4 = new System.Windows.Forms.Button();
+            this.btnResp3 = new System.Windows.Forms.Button();
+            this.btnResp2 = new System.Windows.Forms.Button();
+            this.btnResp1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // btnResp1
-            // 
-            this.btnResp1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnResp1.BackgroundImage")));
-            this.btnResp1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnResp1.Location = new System.Drawing.Point(23, 165);
-            this.btnResp1.Name = "btnResp1";
-            this.btnResp1.Size = new System.Drawing.Size(144, 44);
-            this.btnResp1.TabIndex = 0;
-            this.btnResp1.Text = "button1";
-            this.btnResp1.UseVisualStyleBackColor = true;
-            this.btnResp1.Click += new System.EventHandler(this.btnResp1_Click);
-            // 
-            // btnResp2
-            // 
-            this.btnResp2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnResp2.BackgroundImage")));
-            this.btnResp2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnResp2.Location = new System.Drawing.Point(186, 165);
-            this.btnResp2.Name = "btnResp2";
-            this.btnResp2.Size = new System.Drawing.Size(144, 44);
-            this.btnResp2.TabIndex = 1;
-            this.btnResp2.Text = "button2";
-            this.btnResp2.UseVisualStyleBackColor = true;
-            // 
-            // btnResp3
-            // 
-            this.btnResp3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnResp3.BackgroundImage")));
-            this.btnResp3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnResp3.Location = new System.Drawing.Point(23, 257);
-            this.btnResp3.Name = "btnResp3";
-            this.btnResp3.Size = new System.Drawing.Size(144, 44);
-            this.btnResp3.TabIndex = 2;
-            this.btnResp3.Text = "button3";
-            this.btnResp3.UseVisualStyleBackColor = true;
-            // 
-            // btnResp4
-            // 
-            this.btnResp4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnResp4.BackgroundImage")));
-            this.btnResp4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnResp4.Location = new System.Drawing.Point(186, 257);
-            this.btnResp4.Name = "btnResp4";
-            this.btnResp4.Size = new System.Drawing.Size(144, 44);
-            this.btnResp4.TabIndex = 3;
-            this.btnResp4.Text = "button4";
-            this.btnResp4.UseVisualStyleBackColor = true;
             // 
             // lbPregunta
             // 
@@ -135,6 +90,51 @@ namespace SpaceApp.Forms
             this.pictureBox2.TabIndex = 9;
             this.pictureBox2.TabStop = false;
             // 
+            // btnResp4
+            // 
+            this.btnResp4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnResp4.BackgroundImage")));
+            this.btnResp4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnResp4.Location = new System.Drawing.Point(186, 257);
+            this.btnResp4.Name = "btnResp4";
+            this.btnResp4.Size = new System.Drawing.Size(144, 44);
+            this.btnResp4.TabIndex = 3;
+            this.btnResp4.Text = "button4";
+            this.btnResp4.UseVisualStyleBackColor = true;
+            // 
+            // btnResp3
+            // 
+            this.btnResp3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnResp3.BackgroundImage")));
+            this.btnResp3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnResp3.Location = new System.Drawing.Point(23, 257);
+            this.btnResp3.Name = "btnResp3";
+            this.btnResp3.Size = new System.Drawing.Size(144, 44);
+            this.btnResp3.TabIndex = 2;
+            this.btnResp3.Text = "button3";
+            this.btnResp3.UseVisualStyleBackColor = true;
+            // 
+            // btnResp2
+            // 
+            this.btnResp2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnResp2.BackgroundImage")));
+            this.btnResp2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnResp2.Location = new System.Drawing.Point(186, 165);
+            this.btnResp2.Name = "btnResp2";
+            this.btnResp2.Size = new System.Drawing.Size(144, 44);
+            this.btnResp2.TabIndex = 1;
+            this.btnResp2.Text = "button2";
+            this.btnResp2.UseVisualStyleBackColor = true;
+            // 
+            // btnResp1
+            // 
+            this.btnResp1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnResp1.BackgroundImage")));
+            this.btnResp1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnResp1.Location = new System.Drawing.Point(23, 165);
+            this.btnResp1.Name = "btnResp1";
+            this.btnResp1.Size = new System.Drawing.Size(144, 44);
+            this.btnResp1.TabIndex = 0;
+            this.btnResp1.Text = "button1";
+            this.btnResp1.UseVisualStyleBackColor = true;
+            this.btnResp1.Click += new System.EventHandler(this.btnResp1_Click);
+            // 
             // Fase1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -161,14 +161,13 @@ namespace SpaceApp.Forms
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnResp1;
-        private System.Windows.Forms.Button btnResp2;
-        private System.Windows.Forms.Button btnResp3;
-        private System.Windows.Forms.Button btnResp4;
         private System.Windows.Forms.Label lbPregunta;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnResp4;
+        private System.Windows.Forms.Button btnResp3;
+        private System.Windows.Forms.Button btnResp2;
+        private System.Windows.Forms.Button btnResp1;
     }
 }
