77a1259079be2c2272514616a7bac87cfd1e9fa6
Glozard <kevinjoelmoreira4@gmail.com>
2021-10-03 13:22:03 -0300

    hdsdg

--- SpaceApp/Forms/Inicio.Designer.cs
+++ SpaceApp/Forms/Inicio.Designer.cs
@@ -30,25 +30,10 @@ namespace SpaceApp
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormInicio));
-            this.txtTitulo = new System.Windows.Forms.Label();
             this.btnStart = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // txtTitulo
-            // 
-            this.txtTitulo.AutoSize = true;
-            this.txtTitulo.BackColor = System.Drawing.Color.Black;
-            this.txtTitulo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtTitulo.Font = new System.Drawing.Font("Candara", 26.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
-            this.txtTitulo.ForeColor = System.Drawing.Color.White;
-            this.txtTitulo.Location = new System.Drawing.Point(133, 30);
-            this.txtTitulo.Name = "txtTitulo";
-            this.txtTitulo.Size = new System.Drawing.Size(140, 44);
-            this.txtTitulo.TabIndex = 0;
-            this.txtTitulo.Text = "Spatium";
-            this.txtTitulo.Click += new System.EventHandler(this.label1_Click);
             // 
             // btnStart
             // 
@@ -65,13 +50,13 @@ namespace SpaceApp
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::SpaceApp.Properties.Resources.Infinite_loop_space_gif;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.MaximumSize = new System.Drawing.Size(420, 420);
             this.pictureBox1.MinimumSize = new System.Drawing.Size(420, 420);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(420, 420);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
@@ -81,7 +66,6 @@ namespace SpaceApp
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(416, 393);
-            this.Controls.Add(this.txtTitulo);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.pictureBox1);
             this.MaximizeBox = false;
@@ -94,13 +78,10 @@ namespace SpaceApp
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label txtTitulo;
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
